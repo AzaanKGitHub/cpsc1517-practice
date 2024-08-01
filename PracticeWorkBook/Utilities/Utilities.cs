c363a52213aa6220d50223ea75c31b9762bc8fd1
AzaanKGitHub <akhan78@nait.ca>
2024-08-01 02:07:00 -0600

    Added IsPositive method to Utilities.cs

--- PracticeWorkBook/Utilities/Utilities.cs
+++ PracticeWorkBook/Utilities/Utilities.cs
@@ -2,6 +2,13 @@
 {
     public class Utilities
     {
-
+        ///<summary>
+        /// Test for positive int value 
+        ///</summary>
+        public static bool IsPositive(int value)
+        {
+            // returns true value if value is postive else returns false 
+            return value > 0 ? true: false;
+        }
     }
 }
